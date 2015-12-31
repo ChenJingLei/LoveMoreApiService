fb493e7cd9384ebdd8233e866ca73ec901f21a07
HeZYSaaaaln <kjinks@qq.com>
2015-12-31 19:50:33 +0800

    cjl

--- src/main/java/app/controller/FindController.java
+++ src/main/java/app/controller/FindController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FindController {
 
-    @RequestMapping("/find/test/ssss")
+    @RequestMapping("/find/test/sssss")
     public String test(){
         return "Connect success";
     }
